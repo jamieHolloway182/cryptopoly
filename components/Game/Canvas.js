56225d5f96f949d1ec4f37c08d63f8455d72b3e9
Jamie Holloway <jamie@cadiahq.com>
2022-10-22 15:23:39 +0100

    stuff

--- components/Game/Canvas.js
+++ components/Game/Canvas.js
@@ -10,18 +10,19 @@ const Canvas = () => {
     useEffect(() => {
         canvasRef.current.width = window.innerWidth - 20;
         canvasRef.current.height = window.innerHeight - 100;
+        
+        //gets canvas
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+    
+        //draw background
+        ctx.strokeStyle = '#000000';
+        ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     }, [])
 
     //game run function, runs every frame and updates canvas
     useEffect(() => {
 
-        //gets canvas
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-
-        //draw background
-        ctx.strokeStyle = '#000000';
-        ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     })
 
     return (
