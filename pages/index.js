82ccd74f5e742f8e4537af2b17ff539627905bba
bianca <biancamarcellomilanez@hotmail.com>
2022-10-22 20:17:46 +0100

    byeee

--- pages/index.js
+++ pages/index.js
@@ -9,40 +9,40 @@ import Room from "../components/Layout/Room"
 export default function Home() {
 
     //holds the different rooms
-    const [rooms, setRoom] = useState([
-    {
-      id: nanoid,
-      title: "Lowest",
-      price: "0.000001",
-      color: "#b9a0ee"
-    },
-    {
-      id: nanoid,
-      title: "middle low",
-      color: "#abe591",
-      price: "0.00001"
-    },
-    {
-      id: nanoid,
-      title: "middle high",
-      color: "#ed2226",
-      price: "0.0001"
-    },
-    {
-      id: nanoid,
-      title: "highest",
-      color: "#8d8cdf",
-      price: "0.001" 
-    }
-    ]);
+    // const [rooms, setRoom] = useState([
+    // {
+    //   id: nanoid,
+    //   title: "Lowest",
+    //   price: "0.000001",
+    //   color: "#b9a0ee"
+    // },
+    // {
+    //   id: nanoid,
+    //   title: "middle low",
+    //   color: "#abe591",
+    //   price: "0.00001"
+    // },
+    // {
+    //   id: nanoid,
+    //   title: "middle high",
+    //   color: "#ed2226",
+    //   price: "0.0001"
+    // },
+    // {
+    //   id: nanoid,
+    //   title: "highest",
+    //   color: "#8d8cdf",
+    //   price: "0.001" 
+    // }
+    // ]);
   
   return (
     <div>
-        {rooms.map((room) => (
+        {/* {rooms.map((room) => (
           <Room id={room.id} 
               title={room.title} 
               price={room.price}
-              color={room.color}/>))}
+              color={room.color}/>))} */}
 
     </div>
   )
