4222b1aad3b0374f2e24236ded88622832264ef6
bianca <biancamarcellomilanez@hotmail.com>
2022-10-23 04:01:02 +0100

    some changes

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   
   return (
     <div>
-      <button ref={connectRef}>Connect to MetaMask</button>
+      <button className={styles.button} ref={connectRef}>Connect to MetaMask</button>
     </div>
   )
 }
