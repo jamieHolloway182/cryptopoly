5092fd459cb4c772fdfbdf61d3ec59812d5d9a58
Jamie Holloway <jamie@cadiahq.com>
2022-10-23 07:57:33 +0100

    finish screen

--- components/Game/GameFinishScreen.js
+++ components/Game/GameFinishScreen.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import style from '../../styles/Layout/FinishScreen.module.css'
 
-const GameFinishScreen = () => {
+
+const GameFinishScreen = ( {winner, amount, onCollect, onMoney}) => {
+
+
   return (
     <div>
+        <button className={style.button} onClick={onCollect} >Get Collectable</button>
+        <button className={style.button} onClick={onMoney} >Get Money</button>
         
     </div>
   )
