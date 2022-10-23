62b22c150cba84ddec4f1b708c51c2872ce81f6f
bianca <biancamarcellomilanez@hotmail.com>
2022-10-23 07:14:11 +0100

    big

--- components/Game/Game.js
+++ components/Game/Game.js
@@ -1,9 +1,13 @@
 import roomStyles from '../../styles/Layout/Room.module.css'
+import Image from "next/image"
 
-const Game = ({id, name, click}) => {
+const Game = ({id, name, click, image}) => {
     return(
         <div onClick={click} className={roomStyles.room}>         
-            <span>{name}</span>           
+            <span>{name}</span>     
+            <Image src={image}
+                width={"100%"}
+                height={"150%"}/>
         </div>
     );
 };
--- components/Room.js
+++ components/Room.js
@@ -2,7 +2,7 @@ import roomStyles from '../styles/Layout/Room.module.css'
 
 const Room = ({title, price, color, click}) => {
     return(
-        <div onClick={click.bind(this, price)}className={roomStyles.room}>         
+        <div onClick={click.bind(this, price)} className={roomStyles.room} style={{ backgorundcolor: {color} }}>   
             <span>{title}</span>
             <div>Amount betting: {price}</div>                    
         </div>
